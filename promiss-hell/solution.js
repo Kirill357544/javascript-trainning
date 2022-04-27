912467c452af0f6d0717f56b66c9d93e8f5825cc
Lalov Kirill <kirill.lalov@artezio-training.com>
2022-04-27 09:30:28 +0300

    feat: complete promise-hell task

--- promiss-hell/solution.js
+++ promiss-hell/solution.js
@@ -5,57 +5,46 @@
  * массив-карту коллег по проектам для указанного имени пользователя
  */
 
-// Для получения данных используется объект userService
-// (описание его методов и возвращаемых типов находится в ./api.js)
-
-// function userColleagues(userName) {
-//     return new Promise(function (resolve) {
-//         userService.getUser(userName).then(function (user) {
-//             const userName = user.username;
-//             userService.getUserProjectInfo(user.id).then(function (projectInfo) {
-//                 const colleagueMap = [];
-//                 for (const iterator of projectInfo.projectCodes) {
-//                     userService.getProject(iterator).then(function (project) {
-//                         const projectTitle = project.title;
-//                         userService.getProjectUsers(project.id).then(function (projectUsers) {
-//                             const partners = projectUsers.filter((user) => user !== userName);
-//                             colleagueMap.push({
-//                                 projectTitle,
-//                                 users: partners,
-//                             });
-//                         });
-//                         resolve(colleagueMap);
-//                     });
-//                 }
-//             });
-//         });
-//     });
-// }
-
 const userService = new UserService();
 const username = "izeiner";
 
 function userColleagues(userName) {
-    const promise = new Promise(function (resolve) {
-        resolve(userService.getUser(userName));
-    });
+    const colleagueMap = [];
 
-    promise
+    return userService
+        .getUser(userName)
         .then(function (user) {
             return userService.getUserProjectInfo(user.id);
         })
         .then(function (projectInfo) {
             const codes = [];
 
-            for (const code of projectInfo.projectCodes) {
-                codes.push(new Promise(function(resolve){
-                    
-                }));
+            for (const projectCode of projectInfo.projectCodes) {
+                codes.push(userService.getProject(projectCode));
             }
 
-            Promise.all(codes);
+            return Promise.all(codes).then(function (projects) {
+                return projects;
+            });
+        })
+        .then(function (projects) {
+            const colleagues = [];
+
+            for (const project of projects) {
+                colleagues.push(userService.getProjectUsers(project.id));
+                colleagueMap.push({
+                    projectTitle: project.title,
+                });
+            }
+
+            Promise.all(colleagues).then(function (projectColleagues) {
+                for (let i = 0; i < projectColleagues.length; i++) {
+                    colleagueMap[i].users = projectColleagues[i].filter(
+                        (colleague) => colleague !== userName
+                    );
+                }
+            });
         });
-    return promise;
 }
 
 // Вызов функции возвращает "обещание" с результируемой "картой коллег"
